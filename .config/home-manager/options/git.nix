6c8c22b65cbf96a988ea4ca0094488e484d4dbfa
ano333333 <ano333333github@gmail.com>
2025-09-28 16:22:40 +0900

    feat: .gitconfigでgithubアカウントを分ける

--- .config/home-manager/options/git.nix
+++ .config/home-manager/options/git.nix
@@ -4,4 +4,17 @@
   enable = true;
   userName = "ano333333";
   userEmail = email;
+  extraConfig = {
+    core = {
+      sshCommand = "ssh -i ~/.ssh/id_ed25519";
+    };
+  };
+
+  # プライベートのアカウント用の.gitconfigはincludeIfで読み込む
+  includes = [
+    {
+      condition = "gitdir:~/private/";
+      path = "~/.gitconfig.private";
+    }
+  ];
 }
