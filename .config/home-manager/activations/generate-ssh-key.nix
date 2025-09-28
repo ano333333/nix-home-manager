d9caa283939debcb97805e5e48abc3f890daf13b
ano333333 <ano333333github@gmail.com>
2025-09-28 15:55:31 +0900

    feat: id_ed25519 sshの追加作成

--- .config/home-manager/activations/generate-ssh-key.nix
+++ .config/home-manager/activations/generate-ssh-key.nix
@@ -10,6 +10,8 @@
   SSH_DIR="${config.home.homeDirectory}/.ssh"
   PRIV_KEY_ED25519_PATH="$SSH_DIR/id_ed25519"
   PUB_KEY_ED25519_PATH="$SSH_DIR/id_ed25519.pub"
+  PRIV_KEY_ED25519_2_PATH="$SSH_DIR/id_ed25519_2"
+  PUB_KEY_ED25519_2_PATH="$SSH_DIR/id_ed25519_2.pub"
 
   if [ ! -f "$PRIV_KEY_ED25519_PATH" ]; then
     echo "Generating SSH key(ed25519)..."
@@ -17,5 +19,13 @@
     echo "SSH key(ed25519) generated successfully"
   else
     echo "SSH key(ed25519) already exists"
+  fi
+
+  if [ ! -f "$PRIV_KEY_ED25519_2_PATH" ]; then
+    echo "Generating SSH key(ed25519) 2..."
+    ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -C "${email}" -f "$PRIV_KEY_ED25519_2_PATH" -N ""
+    echo "SSH key(ed25519) 2 generated successfully"
+  else
+    echo "SSH key(ed25519) 2 already exists"
   fi
 ''
