6db4cdc38818b58524ef56e574bbb17f79798e0b
ano333333 <ano333333github@gmail.com>
2025-11-12 01:06:57 +0900

    feat: install zinit

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -58,6 +58,12 @@ in {
 
       # dir for python simlink
       export PATH="$HOME/.bin:$PATH"
+
+      # install zinit
+      ZINIT_HOME="$HOME/.local/share/zinit/zinit.git"
+      [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+      [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+      source "$ZINIT_HOME/zinit.zsh"
     '';
   };
 
