be6a6a77964fb10ce9515f3decc6537e9625dd6b
ano333333 <ano333333github@gmail.com>
2025-11-29 09:17:39 -0800

    feat: nix run .#updateにhome-manager/nix-darwinのアップデートを追加

--- scripts/homeManagerUpdate.sh
+++ scripts/homeManagerUpdate.sh
@@ -2,10 +2,19 @@
 
 set -e
 
+os=$(uname -s)
+
+echo "current OS: $os"
+
 echo "update flake"
 nix flake update
 
 echo "update home-manager"
-nix run nixpkgs#home-manager -- switch --flake .#homeConfiguration
+if [[ $os = "Linux" ]]; then
+  nix run nixpkgs#home-manager -- switch --flake .#linux
+elif [[ $os = "Darwin" ]]; then
+  nix run nixpkgs#home-manager -- switch --flake .#darwin
+  sudo nix run nix-darwin -- switch --flake .#darwin
+fi
 
 echo "update complete"
