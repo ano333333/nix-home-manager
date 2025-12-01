632c14d0709ed1fdf09f7c218f9393aed1f2a14f
ano333333 <ano333333github@gmail.com>
2025-12-02 00:08:19 +0900

    fix: homeManagerUpdate.shのPOXIS対応・shebang修正

--- scripts/homeManagerUpdate.sh
+++ scripts/homeManagerUpdate.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 
 set -e
 
@@ -10,9 +10,9 @@ echo "update flake"
 nix flake update
 
 echo "update home-manager"
-if [[ $os = "Linux" ]]; then
+if [ "$os" = "Linux" ]; then
   nix run nixpkgs#home-manager -- switch --flake .#linux
-elif [[ $os = "Darwin" ]]; then
+elif [ "$os" = "Darwin" ]; then
   nix run nixpkgs#home-manager -- switch --flake .#darwin
   sudo nix run nix-darwin -- switch --flake .#darwin
 fi
