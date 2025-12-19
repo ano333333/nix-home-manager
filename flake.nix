a1b86e8ee22c60d9862466921fd4af6ef79af1fc
ano333333 <ano333333github@gmail.com>
2025-12-20 08:34:28 +0900

    fix: nixpkgsとhome-managerのバージョンを25.11に固定 - unstableだとflake.lockがアップデートされすぎた

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixgl = {
