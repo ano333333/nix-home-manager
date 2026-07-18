4ad17cc5d90b0c2d2e5dae2cc5c587ee85379a3e
ano3 <ano333333github@gmail.com>
2026-07-19 05:58:52 +0900

    feat: waylandFrontend

--- configuration.nix
+++ configuration.nix
@@ -44,7 +44,10 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-mozc-ut fcitx5-gtk ];
+    fcitx5 = {
+      addons = with pkgs; [ fcitx5-mozc-ut fcitx5-gtk ];
+      waylandFrontend = true;
+    };
   };
 
   console.keyMap = "jp106";
